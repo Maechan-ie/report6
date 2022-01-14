3f312ceece4c417a6c919c2c81feaa7bfd5d531b
Maechan-ie <93558697+Maechan-ie@users.noreply.github.com>
2022-01-14 19:22:53 +0900

    Update Roulette.java

--- app/src/main/java/jp/ac/uryukyu/ie/e215735/Roulette.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215735/Roulette.java
@@ -6,12 +6,8 @@ public class Roulette {
        Random rnd = new Random();
        this.bullet = rnd.nextInt(6);/*銃には六発の弾丸を込めることができ、
                                      その中のランダムな場所に弾を込める*/
-        return this.bullet;
-        
-        
+        return this.bullet;    
     }
-    
-
 }
     
 
