0254c954d36fc3467ba577952b8a9efbbdaef55a
Yuugo Maeshiro <e215735@ie.u-ryukyu.ac.jp>
2022-01-14 19:20:45 +0900

    1st

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215735/Roulette.java
@@ -0,0 +1,17 @@
+package jp.ac.uryukyu.ie.e215735;
+import java.util.Random;
+public class Roulette {
+    int bullet;
+    int Revolver(){
+       Random rnd = new Random();
+       this.bullet = rnd.nextInt(6);/*銃には六発の弾丸を込めることができ、
+                                     その中のランダムな場所に弾を込める*/
+        return this.bullet;
+        
+        
+    }
+    
+
+}
+    
+
