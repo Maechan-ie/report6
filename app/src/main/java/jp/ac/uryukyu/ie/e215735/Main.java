eb9fb513ca31aaee6c5eebc629dfa9b311b646bf
Yuugo Maeshiro <e215735@ie.u-ryukyu.ac.jp>
2022-01-18 15:51:24 +0900

    4th

--- app/src/main/java/jp/ac/uryukyu/ie/e215735/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215735/Main.java
@@ -1,8 +1,8 @@
 package jp.ac.uryukyu.ie.e215735;
 public class Main {
     public static void main(String[] args) {
-    
         Person per = new Person();
+        per.Master();
         per.Pleyer();
     }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e215735/Person.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215735/Person.java
@@ -1,8 +1,7 @@
 package jp.ac.uryukyu.ie.e215735;
 import java.util.Scanner;
 public class Person {
-    int money;
-    boolean test;
+    private int money;
     void Pleyer(){
         money = 1000;
         int gan[] = {0,1,2,3,4,5};
@@ -15,28 +14,28 @@ public class Person {
             if(num==1){
                 if(a.getBullet()==s){
                     System.out.println("死にました。");
-                    System.out.println(a.getBullet());
-                    test = true;
                     break;
                 }else{
                 
                     System.out.println("大丈夫でした。");
                     money*=10;
-                    System.out.println("今のお金は"+money+"円です。");
+                    System.out.println("今のお金は"+getMoney()+"円です。");
                 }
             }
             if(num==2){
-                System.out.println(money+"円ゲット");
+                System.out.println(getMoney()+"円ゲット");
                 break;
             }
         }
         
     }
-    
-    void Employee(){
+    int getMoney(){
+        return money;
+    }
+    void Master(){
         System.out.println("ようこそロシアンルーレット会場へ");
         System.out.println("ここでは銃を一発打つごとに賭け金が10倍になっていきます");
         System.out.println("打たない選択をとるとその時のお金を全て得ることができます");
-        System.out.println("");
+        System.out.println("誤って死んでしまわないよう気をつけてください");
     }
 }
